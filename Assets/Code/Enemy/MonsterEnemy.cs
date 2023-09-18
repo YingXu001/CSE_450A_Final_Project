b8d7eb98885f620f12744a037b19722f042e2350
Fiona Xu <50728665+YingXu001@users.noreply.github.com>
2023-09-17 20:31:38 -0500

    create the MonsterEnemy

---
+++ Assets/Code/Enemy/MonsterEnemy.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MonsterEnemy : MonoBehaviour
+{
+    public float pushBackForce = .2f;  // Force with which the submarine is pushed back, adjust as needed
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        // Check if the colliding object is the submarine (tagged as Player)
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            // Retrieve the Health component of the submarine and decrease its health
+            Health playerHealth = collision.gameObject.GetComponent<Health>();
+            if (playerHealth != null)
+            {
+                playerHealth.DamagePlayer(10);
+            }
+
+            // Push the submarine back in the opposite direction of its movement
+            Rigidbody2D playerRigidbody = collision.gameObject.GetComponent<Rigidbody2D>();
+            if (playerRigidbody != null)
+            {
+                Vector2 pushDirection = -playerRigidbody.velocity.normalized;
+                playerRigidbody.AddForce(pushDirection * pushBackForce, ForceMode2D.Impulse);
+            }
+        }
+    }
+}
