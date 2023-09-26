ff52ec7ea85d62399b28e50b75c3662d673f68a7
a-c-everett <a.c.everett@wustl.edu>
2023-09-26 13:54:35 -0500

    Added transition from lvl1 to lvl2

---
+++ Assets/Code/LevelTransitions/LevelExit.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelExit : MonoBehaviour
+{
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.GetComponent<SubController>())
+        {
+            SceneManager.LoadScene("Lv2");
+        }
+    }
+}
