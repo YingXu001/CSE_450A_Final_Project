122dc24421eff614a6eb20cbade0680e44a73826
Fiona Xu <50728665+YingXu001@users.noreply.github.com>
2023-11-24 23:37:34 -0600

    Add Random Enemies for 3 lvs

--- Assets/Code/Enemy/RandomEnemy.cs
+++ Assets/Code/Enemy/RandomEnemy.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.Tilemaps;
 
@@ -9,10 +10,17 @@ public class RandomEnemy : MonoBehaviour
     public int damage = 20;
     private SubController player;
     private Rigidbody2D playerRigidbody;
+    public bool canMove = true; // Assuming you want to control movement
 
     private void Start()
     {
         player = FindObjectOfType<SubController>();
+        if (player == null)
+        {
+            Debug.LogError("Player not found in the scene.");
+            return;
+        }
+        
         gameObject.layer = LayerMask.NameToLayer("RandomEnemy");
 
         var spriteRenderer = GetComponent<SpriteRenderer>();
@@ -25,31 +33,51 @@ public class RandomEnemy : MonoBehaviour
         Debug.Log("RandomEnemy started at position: " + transform.position);
     }
 
-
     private void OnCollisionEnter2D(Collision2D collision)
     {
-        if (collision.gameObject == player.gameObject)
+        if (collision.gameObject.CompareTag("Player"))
         {
-            HandleCollisionWithPlayer();
+            HandleCollisionWithPlayer(collision.gameObject); // Pass collision.gameObject as the argument
+        }
+        else if (collision.gameObject.CompareTag("Bullet") || collision.gameObject.CompareTag("Freeze-Bullet"))
+        {
+            if (collision.gameObject.CompareTag("Freeze-Bullet"))
+            {
+                canMove = false; // Disable movement if hit by a freeze bullet
+                print("freeze!");
+            }
+            // Since this enemy will be destroyed, no need to check for player null here
+            player.IncreaseEnergy(30);
+            Destroy(gameObject);
         }
     }
 
-    private void HandleCollisionWithPlayer()
+
+    private void HandleCollisionWithPlayer(GameObject playerObj)
     {
-        if (player.HasSheild())
+        SubController playerController = playerObj.GetComponent<SubController>();
+
+        if (playerController != null)
         {
-            player.DeactivateShield();
+            if (playerController.HasSheild()) // Correct the method name to match SubController
+            {
+                playerController.DeactivateShield();
+            }
+            else
+            {
+                playerController.playerHealth.DamagePlayer(damage);
+            }
+
+            Rigidbody2D playerRigidbody = playerObj.GetComponent<Rigidbody2D>();
+            if (playerRigidbody != null)
+            {
+                Vector2 pushDirection = (playerObj.transform.position - transform.position).normalized;
+                playerRigidbody.AddForce(pushDirection * pushBackForce, ForceMode2D.Impulse);
+            }
         }
         else
         {
-            player.playerHealth.DamagePlayer(damage);
-        }
-
-        playerRigidbody = player.GetComponent<Rigidbody2D>();
-        if (playerRigidbody != null)
-        {
-            Vector2 pushDirection = (player.transform.position - transform.position).normalized;
-            playerRigidbody.AddForce(pushDirection * pushBackForce, ForceMode2D.Impulse);
+            Debug.LogError("SubController component not found on playerObj.");
         }
     }
 }
